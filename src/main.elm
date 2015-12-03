bfb3733d38e56842bd1844d72272f02cf7d8e699
Adrian <adicirstei@gmail.com>
2015-12-03 17:29:16 +0200

    skeleton

--- src/main.elm
+++ src/main.elm
@@ -2,9 +2,19 @@ import Html exposing(..)
 
 import Advent.Day1
 
+view : model -> Signal Html
+view model =
+  div [] [ text "abcd" ]
+
+
+start : {model : model, view : model -> Html, update : action -> model -> model } -> Signal Html
+start m v u =
+  Nothing
+
+
 app =
-  { html = div [] [ text "abcd" ] }
+  { html = view () }
 
 
 
-main = app.html
+main = start { model = 0, view = view, update = identity }
