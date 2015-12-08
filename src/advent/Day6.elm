8a635f5567f5a11955a433800ec7b9e776f03df2
Adrian <adicirstei@gmail.com>
2015-12-08 16:55:17 +0200

    lot of work

--- src/advent/Day6.elm
+++ src/advent/Day6.elm
@@ -1,13 +1,102 @@
-module Advent.Day6 (solution, grid, set, get, switch) where
+module Advent.Day6  where
 
 import String
-import Char
+import Regex
 import Array
 
-type Action
-  = On Int Int
-  | Off Int Int
-  | Switch Int Int
+
+type alias Pair = (Int, Int)
+
+type Command
+  = NoOp
+  | TurnOn (Pair, Pair)
+  | TurnOff (Pair, Pair)
+  | Toggle (Pair, Pair)
+
+
+isError : Result x a -> Bool
+isError result =
+  case result of
+    Err _ -> True
+    _ -> False
+
+isNothing : Maybe a -> Bool
+isNothing maybe =
+  case maybe of
+    Nothing -> True
+    _ -> False
+
+
+toInt : Maybe String -> Maybe Int
+toInt str =
+  case str of
+    Nothing -> Nothing
+    Just s -> (Result.toMaybe << String.toInt) s
+
+
+
+toPair : List (Maybe Int) -> Maybe (Int, Int)
+toPair list =
+  case list of
+    (Just a :: Just b :: []) -> Just (a, b)
+    _ -> Nothing
+
+
+listToPairs list =
+  let
+    left = toPair (List.take 2 list)
+    right = toPair (List.drop 2 list)
+  in
+    case (left, right) of
+      (Just a, Just b) -> Just (a, b)
+      _ -> Nothing
+
+
+match : String -> Maybe Regex.Match
+match =
+  let
+    rex = Regex.regex "^([^0-9]+)(\\d+),(\\d)+ through (\\d+),(\\d+)$"
+  in
+    List.head << (Regex.find (Regex.AtMost 1) rex)
+
+
+
+matchToCommand : List (Maybe String) -> Command
+matchToCommand list =
+  case list of
+    (str :: area) ->
+      let
+        coords = listToPairs (List.map toInt area)
+        z = Debug.log "coords" coords
+      in
+        NoOp
+    _ -> NoOp
+
+
+    --     if coords == Nothing then
+    --       NoOp
+    --     else
+    --       Just c ->
+    --         case str of
+    --           "turn on " -> TurnOn c
+    --           "turn off " -> TurnOff c
+    --           "toggle " -> Toggle c
+    --           _ -> NoOp
+    -- _ -> NoOp
+
+
+
+
+commandFromString : String -> Command
+commandFromString str =
+  let
+    m = match str
+  in
+    case m of
+      Nothing -> NoOp
+      Just a -> matchToCommand a.submatches
+
+
 
 
 grid =
